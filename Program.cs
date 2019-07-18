21584ea3b75e08448d3f6884572acec59fa31585
Aaron Whang and Holden Clark <aaron@>
2019-07-18 16:46:48 -0700

    update story

--- Program.cs
+++ Program.cs
@@ -27,19 +27,19 @@ namespace Adventure
       
       Branches X1A1 = new Branches("library", "You enter the library and are overtaken by the sheer number of books. Each shelf is filled to the brim.", "2nd visit", "(bookshelf = 8, living room = 3)", 6, false, false);
       
-      Branches X1A2 = new Branches("mirror room", "You wake up in your bedroom", "2nd visit", "movement choice", 7, false, false);
+      Branches X1A2 = new Branches("mirror room", "You look into the giant mirror. The dust layer is so thick that it prevents it from casting a reflection. You reach out and push aside the ancient dust and grime.", "2nd visit", "(You see yourself smile in the reflection = 9, You see yourself cry = 10)", 7, false, false);
       
       Branches X1A1A = new Branches("bookshelf", "You walk towards the bookshelf. Title after title you recognize; this library feels as though it is yours. All of your favorites, and other stories you've wished to read but previously haven't had the time to. You grab the one book with a title you do not recognize, named 'My Story'. You open it and find that every page is empty white. Everything goes white...", "2nd visit", "", 8, true, false);
       
-      Branches X1A2A = new Branches("happy reflection", "You wake up in your bedroom", "2nd visit", "movement choice", 9, false, false);
+      Branches X1A2A = new Branches("happy reflection", "The mirror shines and reveals an image of paradise. It looks like the mnirror is a window to heaven itself. You reach out yet you cannot get past the glass. Of course you can't. Mirrors do not work that way. You have a feeling that you aren't worthy of such a place. You see that the mirror shifts into an image of the mansion.", "2nd visit", "", 9, true, false);
       
-      Branches X1A2AB = new Branches("sad reflection", "You wake up in your bedroom", "2nd visit", "movement choice", 10, false, false);
+      Branches X1A2AB = new Branches("sad reflection", "The mirror grows dark and reveals an image of suffering. Your face twists in fear. You look around and the walls in the room melt away. Your body begins to fade as the world starts to burn. Everything bocomes hot. Everything becomes dark.", "2nd visit", "", 10, true, false);
       
-      Branches X1B1 = new Branches("cellar", "You wake up in your bedroom", "2nd visit", "movement choice", 11, false, false);
+      Branches X1B1 = new Branches("cellar", "You make your way down the moldy gaping entrance of what you assume is the cellar. The air is thick with rot, fermented many times over. You see another entrane adorned with iron bars that long rotted off the walls. You also see a candlestick hanging out of a wall that looks a little too unique.", "2nd visit", "Go into the dungeon = 13, inspect the candlestick = 15", 11, false, false);
 
       Branches X1B2 = new Branches("yogurt", "A strange presence overtakes you, and you feel as if you've lost all control over your body as you spoon the yogurt ferociously into your mouth... Some time passes and you feel awful, collapsing to the kitchen floor...", "2nd visit", "", 12, true, false);
       
-      Branches X1B1A = new Branches("dungeon", "You wake up in your bedroom", "2nd visit", "movement choice", 13, false, false);
+      Branches X1B1A = new Branches("dungeon", "You enter the family friendly dungeon. Nothing bad ever happened here. A bad guy used to live here and he's a big meany.", "2nd visit", "(Go to the Altar = 14)", 13, false, false);
       
       Branches X1B1A1 = new Branches("altar", "You wake up in your bedroom", "2nd visit", "movement choice", 14, false, false);
       
